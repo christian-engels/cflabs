fb282e92f019f31901556b45acccdec7a3255f1d
Christian Engels <33321115+christian-engels@users.noreply.github.com>
2025-09-07 18:00:26 +0000

    7 Sept 2025, 19:00

---
+++ save.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+def is_codespaces():
+    return os.environ.get("CODESPACES") == "true" or "CODESPACE_NAME" in os.environ
+
+if is_codespaces():
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", "Saved from GitHub Codespaces"], check=True)
+    subprocess.run(["git", "push"], check=True)
+    
